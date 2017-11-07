cdc16d84f4d137693c5d21203c23f5148584eeff
Julien Rialland <julien.rialland@gmail.com>
2017-11-08 00:41:27 +0100

    looking for a resolution algorithm

--- parser/src/main/java/net/jr/parser/impl/LazySet.java
+++ parser/src/main/java/net/jr/parser/impl/LazySet.java
@@ -24,18 +24,80 @@ public abstract class LazySet {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LazySet.class);
 
-    private static final Logger getLog() {
-        return LOGGER;
-    }
-
     private Symbol subject;
 
     private Set<LazySet> composition = new HashSet<>();
 
     private Set<Symbol> resolution;
 
+    private Stack<LazySet> redefs = new Stack<>();
+
+    private Stack<Boolean> bRedefs = new Stack<>();
+
     public LazySet(Symbol subject) {
         this.subject = subject;
+    }
+
+    private static final Logger getLog() {
+        return LOGGER;
+    }
+
+    public static void resolveAll(Map<Symbol, FollowSet> map) {
+        System.out.println(String.format("%d/%d", map.values().stream().filter(s->s.isResolved()).count(), map.size()));
+        int total = map.size(), lastResolved=0, resolved = simplify(map.values());
+        while(resolved < total && lastResolved != resolved) {
+            lastResolved = resolved;
+        }
+        System.out.println(String.format("%d/%d", resolved, total));
+    }
+
+    /**
+     * Tries to resolve all the passed-in definitions
+     *
+     * @param map definitions
+     * @throws RuntimeException when it fails at finding a resolution for every sets
+     */
+    private static int simplify(Collection<FollowSet> sets) {
+        final EvtSupport bus = new EvtSupport();
+        for (LazySet ls : sets) {
+            if (!ls.isResolved()) {
+                Set<LazySet> toResolve = ls.composition.stream().filter(s -> !s.isResolved()).collect(Collectors.toSet());
+                bus.addListener(lazySet -> {
+                    toResolve.remove(lazySet);
+                    if (toResolve.isEmpty() && !ls.isResolved()) {
+                        ls.setResolved();
+                        bus.emit(ls);
+                    }
+                });
+            }
+        }
+        int count = 0;
+        for (LazySet ls : sets) {
+            ls.getComposition().remove(ls);
+            if (ls.isResolved()) {
+                bus.emit(ls);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    protected void pushAltDefinition(LazySet def) {
+        redefs.add(def);
+        if (resolution == null && composition.remove(def)) {
+            bRedefs.add(true);
+            composition.addAll(def.getComposition());
+        } else {
+            bRedefs.add(false);
+        }
+    }
+
+    protected void popAltDefinition() {
+        LazySet def = redefs.pop();
+        if (bRedefs.pop()) {
+            composition.removeAll(def.getComposition());
+            composition.add(def);
+        }
     }
 
     public Symbol getSubject() {
@@ -50,89 +112,26 @@ public abstract class LazySet {
         composition.add(lazySet);
     }
 
-    public void setResolution(Set<Symbol> resolution) {
-        this.resolution = resolution;
-    }
-
     public boolean isResolved() {
         return resolution != null;
     }
 
     public Set<Symbol> getResolution() {
-        if(resolution == null) {
-            throw new IllegalStateException("Could not determine terminals for " + this.toString()+ " = " + compositionToString());
+        if (resolution == null) {
+            throw new IllegalStateException("Could not determine terminals for " + this.toString() + " = " + compositionToString());
         }
         return resolution;
     }
 
+    ;
 
-    private static class Bus<E>{
-
-        private Set<Consumer<E>> listeners = new HashSet<>();
-
-        public void emit(E evt) {
-            for(Consumer<E> consumer : listeners) {
-                consumer.accept(evt);
-            }
-        }
-
-        public void addListener(Consumer<E> listener) {
-            listeners.add(listener);
-        }
-
-    };
-
-    public static void resolveAll(Map<Symbol, FollowSet> map) {
-        _resolveAll(map);
-
-    }
-    /**
-     * Tries to resolve all the passed-in definitions
-     *
-     * @param map definitions
-     * @throws RuntimeException when it fails at finding a resolution for every sets
-     */
-    private static void _resolveAll(Map<Symbol, FollowSet> map) {
-
-
-        final Bus bus = new Bus();
-
-        Set<FollowSet> propositions = new HashSet<>();
-
-        for(LazySet ls : map.values()) {
-
-            if(ls.composition.size() == 1) {
-                bus.addListener(lazySet -> {
-                    if(lazySet == ls) {
-                        LazySet compo = ls.getComposition().iterator().next();
-                        compo.setResolution(ls.resolution);
-                        bus.emit(compo);
-                    }
-                });
-            }
-
-            if(!ls.isResolved()) {
-                Set<LazySet> toResolve = ls.composition.stream().filter(s->!s.isResolved()).collect(Collectors.toSet());
-                bus.addListener(lazySet -> {
-                        toResolve.remove(lazySet);
-                        if(toResolve.isEmpty() && !ls.isResolved()) {
-                            ls.setResolved();
-                            bus.emit(ls);
-                        }
-                    });
-            }
-        }
-
-        for(LazySet ls : map.values()) {
-            if(ls.isResolved()) {
-                bus.emit(ls);
-            }
-        }
+    public void setResolution(Set<Symbol> resolution) {
+        this.resolution = resolution;
     }
 
     private void setResolved() {
         resolution = new HashSet<>();
-        for(LazySet ls : composition) {
+        for (LazySet ls : composition) {
             resolution.addAll(ls.getResolution());
         }
     }
@@ -153,5 +152,21 @@ public abstract class LazySet {
                 return String.join(" ∪ ", composition.stream().map(s -> s.toString() + (s.isResolved() ? "*" : "")).collect(Collectors.toList()));
             }
         }
+    }
+
+    private static class EvtSupport<E> {
+
+        private Set<Consumer<E>> listeners = new HashSet<>();
+
+        public void emit(E evt) {
+            for (Consumer<E> consumer : listeners) {
+                consumer.accept(evt);
+            }
+        }
+
+        public void addListener(Consumer<E> listener) {
+            listeners.add(listener);
+        }
+
     }
 }
